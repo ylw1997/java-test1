d04f419a97f454204f639a600bbd1290a3ed1799
Yangliwei <1280426581@qq.com>
2022-03-03 15:54:18 +0800

    添加字段

---
+++ src/main/java/com/yangliwei/test1/config/MyMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package com.yangliwei.test1.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 自动添加日期
+ * @author ylw12
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject,"createTime", Date.class,new Date());
+        this.strictInsertFill(metaObject,"updateTime", Date.class,new Date());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject,"updateTime",Date.class,new Date());
+    }
+}
--- src/main/java/com/yangliwei/test1/model/User.java
+++ src/main/java/com/yangliwei/test1/model/User.java
@@ -1,6 +1,10 @@
 package com.yangliwei.test1.model;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
+
+import java.util.Date;
 
 /**
  * @author ylw12
@@ -11,4 +15,10 @@ public class User {
     private String name;
     private Integer age;
     private String email;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
 }
